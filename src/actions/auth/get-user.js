64d975de24efe9e96a8c3e4a56cce85d4efe0bf8
emreturgutce <emrebursalee@gmail.com>
2021-01-03 20:10:03 +0300

    js-cookie deleted

--- src/actions/auth/get-user.js
+++ src/actions/auth/get-user.js
@@ -1,38 +1,31 @@
-import Cookies from 'js-cookie';
 import { AuthActionTypes } from '../types';
 import { BASE_URL } from '../../constants/index';
 
 export const getUser = (data) => (dispatch) => {
-    const authToken = Cookies.get('auth_token');
+    dispatch({ type: AuthActionTypes.USER_LOADING });
 
-    if (!authToken) {
-        dispatch({ type: AuthActionTypes.AUTH_FAIL });
-    } else {
-        dispatch({ type: AuthActionTypes.USER_LOADING });
-
-        fetch(`${BASE_URL}/api/personels/current`, {
-            method: 'GET',
-            credentials: 'include',
-        })
-            .then((res) => res.json())
-            .then((res) => {
-                if (res.data) {
-                    setTimeout(() => {
-                        dispatch({
-                            type: AuthActionTypes.AUTH_SUCCESS,
-                            payload: res.data[0],
-                        });
-                    }, 2000);
-                } else {
+    fetch(`${BASE_URL}/api/personels/current`, {
+        method: 'GET',
+        credentials: 'include',
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.data) {
+                setTimeout(() => {
                     dispatch({
-                        type: AuthActionTypes.AUTH_FAIL,
+                        type: AuthActionTypes.AUTH_SUCCESS,
+                        payload: res.data[0],
                     });
-                }
-            })
-            .catch((err) => {
+                }, 2000);
+            } else {
                 dispatch({
                     type: AuthActionTypes.AUTH_FAIL,
                 });
+            }
+        })
+        .catch((err) => {
+            dispatch({
+                type: AuthActionTypes.AUTH_FAIL,
             });
-    }
+        });
 };
