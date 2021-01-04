ea393b2652b18e3d5a26be439651de4c4e1fe420
emreturgutce <emrebursalee@gmail.com>
2021-01-04 15:59:13 +0300

    logout action

--- src/actions/auth/logout.js
+++ src/actions/auth/logout.js
@@ -1,13 +1,17 @@
 import { BASE_URL } from '../../constants/index';
 import { AuthActionTypes } from '../types';
 
-export const logout = () => async (dispatch) => {
-    await fetch(`${BASE_URL}/api/personels/logout`, {
+export const logout = () => (dispatch) => {
+    fetch(`${BASE_URL}/api/personels/logout`, {
         method: 'GET',
         credentials: 'include',
-    });
-
-    dispatch({
-        type: AuthActionTypes.LOGOUT_SUCCESS,
-    });
+    })
+        .then(() => {
+            dispatch({
+                type: AuthActionTypes.LOGOUT_SUCCESS,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
