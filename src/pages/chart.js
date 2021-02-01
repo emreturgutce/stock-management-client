e7dc0e27909d7ff349527fdcdedda755368c72b0
emreturgutce <emrebursalee@gmail.com>
2021-02-01 11:58:13 +0300

    profile and chart pages titles

--- src/pages/chart.js
+++ src/pages/chart.js
@@ -1,5 +1,6 @@
 import { useEffect, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
+import Helmet from 'helmet';
 import {
 	LineChart,
 	Line,
@@ -20,30 +21,35 @@ const Chart = () => {
 	useEffect(getSalesCb, [getSalesCb]);
 
 	return (
-		<div
-			style={{
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-				width: '90%',
-				margin: '60px auto',
-			}}
-		>
-			<h3>Satış Grafiği</h3>
-			<LineChart
-				width={800}
-				height={400}
-				data={sales}
-				margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+		<>
+			<Helmet>
+				<title>Satış Grafiği - Stok Yönetim Sistemi</title>
+			</Helmet>
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+					width: '90%',
+					margin: '60px auto',
+				}}
 			>
-				<Line type='monotone' dataKey='count' stroke='#8884d8' />
-				<CartesianGrid stroke='#ccc' strokeDasharray='3 3' />
-				<XAxis dataKey='sale_date' />
-				<YAxis dateKey='count' />
-				<Tooltip />
-				<Legend />
-			</LineChart>
-		</div>
+				<h3>Satış Grafiği</h3>
+				<LineChart
+					width={800}
+					height={400}
+					data={sales}
+					margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+				>
+					<Line type='monotone' dataKey='count' stroke='#8884d8' />
+					<CartesianGrid stroke='#ccc' strokeDasharray='3 3' />
+					<XAxis dataKey='sale_date' />
+					<YAxis dateKey='count' />
+					<Tooltip />
+					<Legend />
+				</LineChart>
+			</div>
+		</>
 	);
 };
 
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,25 +1,32 @@
+import Helmet from 'helmet';
+
 const Profile = () => {
 	return (
-		<div
-			style={{
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-				width: '80%',
-				margin: '60px auto',
-			}}
-		>
+		<>
+			<Helmet>
+				<title>Profil - Stok Yönetim Sistemi</title>
+			</Helmet>
 			<div
 				style={{
-					width: '100%',
 					display: 'flex',
-					flexDirection: 'row',
-					justifyContent: 'space-between',
+					flexDirection: 'column',
+					alignItems: 'center',
+					width: '80%',
+					margin: '60px auto',
 				}}
 			>
-				<h1>Profile</h1>
+				<div
+					style={{
+						width: '100%',
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+					}}
+				>
+					<h1>Profile</h1>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
