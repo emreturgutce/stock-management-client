09309f282e68867968b823725b4744f7af714e7c
Emre Turgut <emrebursalee@gmail.com>
2021-02-18 14:58:36 +0300

    error alert change

--- src/components/error-alert.js
+++ src/components/error-alert.js
@@ -2,12 +2,23 @@ import { useState } from 'react';
 import { Alert, AlertTitle } from '@material-ui/lab';
 import { Collapse } from '@material-ui/core';
 
-const ErrorAlert = ({ errors }) => {
+const ErrorAlert = ({ errors, show, setShow }) => {
 	const [open, setOpen] = useState(true);
 
+	if (!show) {
+		return (
+			<Collapse in={open}>
+				<Alert severity='error' onClose={() => setOpen(false)}>
+					<AlertTitle>Error</AlertTitle>
+					{errors.map((error) => error)}
+				</Alert>
+			</Collapse>
+		);
+	}
+
 	return (
-		<Collapse in={open}>
-			<Alert severity='error' onClose={() => setOpen(false)}>
+		<Collapse in={show}>
+			<Alert severity='error' onClose={() => setShow(false)}>
 				<AlertTitle>Error</AlertTitle>
 				{errors.map((error) => error)}
 			</Alert>
