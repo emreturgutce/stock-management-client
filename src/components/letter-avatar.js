854cf5b7590c6323be5ecbf1b90d335cb58f9d57
emreturgutce <emrebursalee@gmail.com>
2021-02-28 15:19:47 +0300

    dropdown arrows

--- src/components/letter-avatar.js
+++ src/components/letter-avatar.js
@@ -9,10 +9,31 @@ import {
 	Menu,
 	MenuItem,
 	Typography,
+	withStyles,
 } from '@material-ui/core';
 import { AccountCircle, ExitToApp } from '@material-ui/icons';
 import { deepOrange } from '@material-ui/core/colors';
 import { logout } from '../actions';
+
+const StyledMenu = withStyles({
+	paper: {
+		border: '1px solid #d3d4d5',
+	},
+})((props) => (
+	<Menu
+		elevation={0}
+		getContentAnchorEl={null}
+		anchorOrigin={{
+			vertical: 'bottom',
+			horizontal: 'start',
+		}}
+		transformOrigin={{
+			vertical: 'top',
+			horizontal: 'center',
+		}}
+		{...props}
+	/>
+));
 
 const useStyles = makeStyles((theme) =>
 	createStyles({
@@ -49,7 +70,7 @@ export default function LetterAvatar({ firstLetter }) {
 			<Button type='button' onClick={handleClick}>
 				<Avatar className={classes.orange}>{firstLetter}</Avatar>
 			</Button>
-			<Menu
+			<StyledMenu
 				id='simple-menu'
 				anchorEl={anchorEl}
 				keepMounted
@@ -73,7 +94,7 @@ export default function LetterAvatar({ firstLetter }) {
 					/>
 					<Typography variant='span'>Çıkış</Typography>
 				</MenuItem>
-			</Menu>
+			</StyledMenu>
 		</div>
 	);
 }
