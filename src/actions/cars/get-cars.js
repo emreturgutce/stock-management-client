c9a3af6c817cb08bc049c6bcd4427fb203d070da
Emre Turgut <emrebursalee@gmail.com>
2021-03-17 17:39:08 +0300

    get-cars timeout removed

--- src/actions/cars/get-cars.js
+++ src/actions/cars/get-cars.js
@@ -2,32 +2,30 @@ import { BASE_URL } from '../../constants/index';
 import { CarActionTypes } from '../types';
 
 export const getCars = () => (dispatch) => {
-    dispatch({
-        type: CarActionTypes.CARS_LOADING,
-    });
+	dispatch({
+		type: CarActionTypes.CARS_LOADING,
+	});
 
-    fetch(`${BASE_URL}/api/cars`, {
-        method: 'GET',
-        credentials: 'include',
-    })
-        .then((res) => res.json())
-        .then(({ data }) => {
-            if (data) {
-                setTimeout(() => {
-                    dispatch({
-                        type: CarActionTypes.CARS_LOADED,
-                        payload: data,
-                    });
-                }, 500);
-            } else {
-                dispatch({
-                    type: CarActionTypes.CARS_FAIL,
-                });
-            }
-        })
-        .catch((err) => {
-            dispatch({
-                type: CarActionTypes.CARS_FAIL,
-            });
-        });
+	fetch(`${BASE_URL}/api/cars`, {
+		method: 'GET',
+		credentials: 'include',
+	})
+		.then((res) => res.json())
+		.then(({ data }) => {
+			if (data) {
+				dispatch({
+					type: CarActionTypes.CARS_LOADED,
+					payload: data,
+				});
+			} else {
+				dispatch({
+					type: CarActionTypes.CARS_FAIL,
+				});
+			}
+		})
+		.catch((err) => {
+			dispatch({
+				type: CarActionTypes.CARS_FAIL,
+			});
+		});
 };
