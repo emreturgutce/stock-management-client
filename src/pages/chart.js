ca5afe44f046f192172e628700c256344d222629
Emre Turgut <emrebursalee@gmail.com>
2021-03-22 17:57:13 +0300

    using sale-worth-chart

--- src/pages/chart.js
+++ src/pages/chart.js
@@ -23,6 +23,8 @@ import {
 	getTotalProfit,
 	getTotalCustomer,
 	getLatestSales,
+	getTotalSaleMonthly,
+	getTotalWorthMonthly,
 } from '../actions';
 import { useCarState } from '../hooks/use-car-state';
 import TotalProfit from '../components/total-profit';
@@ -32,6 +34,7 @@ import Page from '../components/page';
 import { formatPrice } from '../utils/format-price';
 import { getTotalRevenue } from '../actions/cars/get-total-revenue';
 import LChart from '../components/line-chart';
+import SaleWorthChart from '../components/sale-worth-chart';
 import DateRangeSlider from '../components/date-range-slider';
 
 const fromDate = '2020-06-01';
@@ -52,6 +55,12 @@ const Chart = () => {
 	const getTotalRevenueCb = useCallback(() => dispatch(getTotalRevenue()), [
 		dispatch,
 	]);
+	const getTotalSaleMonthlyCb = useCallback(() => dispatch(getTotalSaleMonthly()), [
+		dispatch,
+	]);
+	const getTotalWorthMonthlyCb = useCallback(() => dispatch(getTotalWorthMonthly()), [
+		dispatch,
+	]);
 	const mediaMatch = window.matchMedia('(min-width: 1200px)');
 	const [matches, setMatches] = useState(mediaMatch.matches);
 
@@ -68,6 +77,8 @@ const Chart = () => {
 	useEffect(getTotalProfitCb, [getTotalProfitCb]);
 	useEffect(getTotalCustomerCb, [getTotalCustomerCb]);
 	useEffect(getTotalRevenueCb, [getTotalRevenueCb]);
+	useEffect(getTotalWorthMonthlyCb, [getTotalWorthMonthlyCb]);
+	useEffect(getTotalSaleMonthlyCb, [getTotalSaleMonthlyCb]);
 
 	const { latestSales } = useCarState();
 
@@ -236,6 +247,38 @@ const Chart = () => {
 							</Card>
 						</Grid>
 					</Grid>
+					<Grid
+						item
+						container
+						justify='center'
+						direction='row'
+						style={{ marginTop: '1rem' }}
+						spacing={1}
+					>
+						<Grid item md={6} sm={12} style={{ width: '100%' }}>
+							<Card style={{ height: '100%' }}>
+								<Grid
+									container
+									direction='column'
+									justify='space-between'
+									style={{ height: '100%' }}
+								>
+									<Grid item>
+										<CardHeader title='Satış Grafiği' />
+
+										<Divider />
+									</Grid>
+									<Grid item>
+										<SaleWorthChart />
+									</Grid>
+									<Grid item style={{ height: 70.8 }}>
+										<Divider />
+										<Box height='100%' />
+									</Grid>
+								</Grid>
+							</Card>
+						</Grid>
+						</Grid>
 				</Grid>
 			</Container>
 		</Page>
