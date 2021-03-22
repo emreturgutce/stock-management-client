656e08f9ff44a1d3ebb3b94e5a8925309ae5397e
Emre Turgut <emrebursalee@gmail.com>
2021-03-22 17:56:48 +0300

    total sale monthly and total worth monthly

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,3 +13,5 @@ export * from './auth/logout';
 export * from './cars/get-latest-sales'
 export * from './cars/get-awaiting-events'
 export * from './cars/get-completed-events'
+export * from './cars/get-total-sale-monthly'
+export * from './cars/get-total-worth-monthly'
--- src/reducers/car-reducer.js
+++ src/reducers/car-reducer.js
@@ -14,6 +14,8 @@ const initialState = {
 	totalRevenue: 0,
 	awaitingEvents: [],
 	completedEvents: [],
+	monthlyTotalWorth: [],
+	monthlyTotalSale: [],
 	errors: [],
 };
 
@@ -218,6 +220,44 @@ export function carReducer(state = initialState, action) {
 				latestSales: action.payload,
 			};
 		case CarActionTypes.LATEST_SALES_FAIL:
+			return {
+				...state,
+				isLoading: false,
+				errors: action.payload?.error
+					? [action.payload?.error]
+					: state.errors,
+			};
+		case CarActionTypes.TOTAL_SALE_MONTHLY_LOADING:
+			return {
+				...state,
+				isLoading: true,
+			};
+		case CarActionTypes.TOTAL_SALE_MONTHLY_LOADED:
+			return {
+				...state,
+				isLoading: false,
+				monthlyTotalSale: action.payload,
+			};
+		case CarActionTypes.TOTAL_SALE_MONTHLY_FAIL:
+			return {
+				...state,
+				isLoading: false,
+				errors: action.payload?.error
+					? [action.payload?.error]
+					: state.errors,
+			};
+		case CarActionTypes.TOTAL_WORTH_MONTHLY_LOADING:
+			return {
+				...state,
+				isLoading: true,
+			};
+		case CarActionTypes.TOTAL_WORTH_MONTHLY_LOADED:
+			return {
+				...state,
+				isLoading: false,
+				monthlyTotalWorth: action.payload,
+			};
+		case CarActionTypes.TOTAL_WORTH_MONTHLY_FAIL:
 			return {
 				...state,
 				isLoading: false,
