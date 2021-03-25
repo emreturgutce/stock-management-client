dd04ccd955ee47f43845de45b59112b7d8f84c43
Emre Turgut <emrebursalee@gmail.com>
2021-03-25 14:00:01 +0300

    fix

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import Page from '../components/page';
